0b7d375ea7b82b080dc3076ab5b46e373eac6035
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2026-03-11 22:41:33 +0800

    Update Multiple_Regression.py

--- Supervised_Learning_with_scikit-learn/Multiple_Regression.py
+++ Supervised_Learning_with_scikit-learn/Multiple_Regression.py
@@ -13,3 +13,17 @@ reg.fit(X_train, y_train)
 # Make predictions
 y_pred = reg.predict(X_test)
 print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+
+---
+# Import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
+
+# Compute RMSE
+rmse = root_mean_squared_error(y_test, y_pred)
+
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
